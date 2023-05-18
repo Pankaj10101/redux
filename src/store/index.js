868591cb1878656b3f7f7f010607e8a9ef3c58d1
Pankaj10101 <rkay1048@gmail.com>
2023-05-18 18:26:01 +0530

    increament and decrement by 5

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,10 @@ import {legacy_createStore as createStore } from 'redux'
 const counterReducer = (state={counter:0}, action)=>{
     switch(action.type){
         case 'INCREMENT' : {
-            return{ counter : state.counter+1}
+            return{ counter : state.counter+5}
         }
         case 'DECREMENT' : {
-            return{ counter : state.counter-1}
+            return{ counter : state.counter-5}
         }
         default : return state
     }
